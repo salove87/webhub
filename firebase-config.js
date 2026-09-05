64fa4d473a914798b9cbdb6eceb6e383923ca348
salove87 <salove3@gmail.com>
2026-09-05 21:51:42 +0700

    Fix article admin loading after Firebase initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,7 @@
 export const firebaseConfig={apiKey:"AIzaSyDZSY0jxEGzTG0rualNAgH_Ly45Ve_b3SY",authDomain:"jobhub-a0de5.firebaseapp.com",projectId:"jobhub-a0de5",storageBucket:"jobhub-a0de5.firebasestorage.app",messagingSenderId:"390768908775",appId:"1:390768908775:web:1bd01cba98df640e228542",measurementId:"G-2W4VFTTKJB"};
 if(location.pathname.endsWith('/admin.html')){
-  import('./admin-company-email-patch.js?v=20260905-1');
-  import('./article-admin-v2.js?v=20260905-1');
+  setTimeout(()=>{
+    import('./admin-company-email-patch.js?v=20260905-1');
+    import('./article-admin-v2.js?v=20260905-2');
+  },0);
 }
