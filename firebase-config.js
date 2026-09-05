c18a120361770c255fb6575ba232be6941303f73
salove87 <salove3@gmail.com>
2026-09-05 10:15:46 +0700

    feat: add firebase-config.js for job marketplace

---
+++ firebase-config.js
@@ -0,0 +1,1 @@
+export const firebaseConfig={apiKey:"AIzaSyDZSY0jxEGzTG0rualNAgH_Ly45Ve_b3SY",authDomain:"jobhub-a0de5.firebaseapp.com",projectId:"jobhub-a0de5",storageBucket:"jobhub-a0de5.firebasestorage.app",messagingSenderId:"390768908775",appId:"1:390768908775:web:1bd01cba98df640e228542",measurementId:"G-2W4VFTTKJB"};
